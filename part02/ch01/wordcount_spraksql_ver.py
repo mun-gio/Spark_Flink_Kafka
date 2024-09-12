948a909b52a4bef05ca8edd0df4777d4defac4ce
mun-gio <mungio@DESKTOP-4SVFRMS>
2024-09-12 13:47:46 +0900

    create wordcount_sparksql_ver.py

---
+++ part02/ch01/wordcount_spraksql_ver.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+
+if __name__ == "__main__":
+    ss: SparkSession = SparkSession.builder \
+        .master("local") \
+        .appName("wordCount RDD ver") \
+        .getOrCreate()
+
+    df = ss.read.text("data/words.txt")
+
+    # transformation
+    df = df.withColumn("word",
+                    f.explode(f.split(f.col("value"), " ")))\
+                    .withColumn("count", f.lit(1)).groupby("word").sum()
+    df.show()
